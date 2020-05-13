01562d9018f725d2706394b4e977e07530afd97c
John Mwaura <johnmwaura08@gmail.com>
2020-05-12 20:24:00 -0500

    rendering the burger

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import Aux from "../../Hoc/Aux";
+import Burger from '../../Components/Burger/Burger';
 
 export class BurgerBuilder extends Component {
   render() {
     return (
       <Aux>
-        <div>Burger</div>
+        {/* // this is where we'll add the burger component from the stateless folder */}
+        <Burger /> 
         <div> Build Controls</div>
       </Aux>
     );
