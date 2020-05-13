656af18497782f3f26789b93e70b26cd95cb6d61
John Mwaura <johnmwaura08@gmail.com>
2020-05-12 21:12:20 -0500

    added object.keys to transform object into iterable arrray

--- src/Containers/BurgerBuilder/BurgerBuilder.js
+++ src/Containers/BurgerBuilder/BurgerBuilder.js
@@ -3,11 +3,25 @@ import Aux from "../../Hoc/Aux";
 import Burger from '../../Components/Burger/Burger';
 
 export class BurgerBuilder extends Component {
+  //this is the modern way of writing class component
+  state ={
+    //we'll have an ingredients object here with key value pairs, 
+    //the keys are the names of the ingredients and the values is the amount
+      ingredients: {
+
+        salad: 1,
+        bacon:2,
+        meat: 2,
+        cheese: 2
+      }
+      //we want to give our burger componet the ingredients
+
+  }
   render() {
     return (
       <Aux>
         {/* // this is where we'll add the burger component from the stateless folder */}
-        <Burger /> 
+        <Burger  ingredients={this.state.ingredients}/> 
         <div> Build Controls</div>
       </Aux>
     );
